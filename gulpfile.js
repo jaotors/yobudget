99bd3a977557c6bddfb96f59e6163e53dfba225d
Joshua B. Turingan <turingan.joshua@gmail.com>
2016-08-19 17:52:12 +0800

    adding gulp in development

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
-var elixir = require('laravel-elixir');
+// Define dependencies
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	elixir = require('laravel-elixir'),
+	css = 'public/css',
+	js = 'public/js';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+
+
+
+gulp.task('components', function() {
+	gulp.src('node_modules/jquery/dist/jquery.min.js')
+		.pipe(gulp.dest(js));
+});
+
+gulp.task('styles', function() {
+	gulp.src('resources/assets/sass/app.scss')
+		.pipe(sass()).on('error', function(error) {console.log('ERROR ' + error.message + '\n\n');})
+		.pipe(autoprefixer({browsers: ['last 2 versions']}))
+		.pipe(gulp.dest('public/css'));
+});
+
+gulp.task('scripts', ['styles'], function() {
+	gulp.src(['resources/assets/js/script.js'])
+		.pipe(gulp.dest('public/js'));
+});
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	
+	mix.task('components');
+	mix.task('styles', 'resources/assets/sass/*.scss');
+	mix.task('scripts', 'resources/assets/js/*.js');
+	mix.browserSync({
+		files: ['resources']
+	});
 });
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Yo! Budget</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+    <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.14.0.js'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+    </body>
+</html>
